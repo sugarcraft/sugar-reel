8ca5a8e42bff8a518dfd962bda274a6178b740ab
Joe Huss <detain@interserver.net>
2026-05-07 05:32:18 -0400

    Add French locale for candy-flip (#245)

---
+++ candy-flip/lang/fr.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * French translations for candy-flip.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'decoder.no_file'   => 'candy-flip : fichier introuvable : {path}',
+    'decoder.no_gd'     => 'candy-flip : ext-gd est requis',
+    'decoder.not_gif'   => 'candy-flip : ce n\'est pas un GIF',
+    'cli.usage'         => 'usage : candy-flip <gif> [solid|density]',
+    'cli.no_autoload'   => 'candy-flip : impossible de trouver composer autoload.php',
+];
